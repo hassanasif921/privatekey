049e3aeb7708b395ac13e2f65a5b8fc7de4e6963
Your Name <hassanasif302672@gmail.com>
2023-10-07 03:55:03 +0500

    first commits

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
+app.get('/get', (req, res) => {
+    res.send('Hello World!');
+});
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
